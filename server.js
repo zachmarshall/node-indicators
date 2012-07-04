399882928b19a36665c32389f2f79fd637e60f99
kurmbox <bluedude@gmail.com>
2012-07-04 13:27:52 -0400

    Specify image size as query parm

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var http = require('http'),
     raphael = require('node-raphael'),
-	url = require('url');
+	url = require('url'),
+	im = require('imagemagick'),
+	fs = require('fs');
 
 var server = http.createServer(function(req, res) {
-    res.writeHead(200, {"Content-Type": "image/svg+xml"});
+    res.writeHead(200, {"Content-Type": "image/png"});
 	
 	var url_parts = url.parse(req.url, true);
 	var query = url_parts.query;
 	
   	var percent = query["percent"] ? parseFloat(query["percent"]) / 100.0 : 0.0;
+	var size = query["size"] ? parseFloat(query["size"]) : 58;
   	var outerStroke = 3;
   	var innerStroke = 2;
   	var radius = 50.0;
@@ -37,7 +40,19 @@ var server = http.createServer(function(req, res) {
 		}
 		meter.glow({color:"#fff", width: glow});
     });
-    res.end(svg);
+	var conv = im.convert(['-background', 'none',
+				'-resize', size,
+				'-density', '220', 
+				'-units', 'PixelsPerInch', 
+				'svg:-', 'png:-']);
+	conv.stdout.on('data', function(chunk){
+		res.write(chunk, 'binary');
+	});
+	conv.stdout.on('end', function(){
+		res.end();
+	});
+	conv.stdin.write(svg);
+	conv.stdin.end();
 });
 
 server.listen(parseInt(process.env.PORT) || 3000);
